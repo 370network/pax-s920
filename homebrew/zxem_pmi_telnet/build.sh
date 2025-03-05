28d235faa2d5e6e2c66141cffec967f00d55c10f
Richard Gráčik - Morc <r.gracik@gmail.com>
2025-03-05 15:25:29 +0100

    homebrew: add original hax-pax

---
+++ homebrew/zxem_pmi_telnet/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# build.sh - Morc @ 370network
+source $PREFIX/../homebrew/build_helper.sh
+
+echo "=================="
+echo "370network paxdevs"
+echo "= build hax-pax  ="
+echo "=================="
+
+git clone https://github.com/Sprotex/hax-pax/ repo
+
+if [ ! -f repo/makefileupdated ]; then
+	cp Makefile repo/Makefile
+	echo "done" > repo/makefileupdated
+fi
+
+if [ ! -f repo/zxem ]; then
+	echo "zxem build process!"
+	make -j$(nproc) -C repo zxem
+fi
+
+if [ ! -f repo/pmi ]; then
+        echo "pmi build process!"
+        make -j$(nproc) -C repo pmi
+fi
+
+if [ ! -f repo/telnet ]; then
+        echo "telnet build process!"
+        make -j$(nproc) -C repo telnet
+fi
+
+build_check "zxem" "zxem"
+build_check "pmi" "pmi"
+build_check "telnet" "telnet"
