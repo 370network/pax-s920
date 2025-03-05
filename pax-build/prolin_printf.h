e7f81429cbfc6f73f67e24de178faec983484645
Richard Gráčik - Morc <r.gracik@gmail.com>
2025-03-05 12:26:49 +0100

    dir restructure 2

--- pax-build/prolin_printf.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROLIN_PRINTF_H
-#define PROLIN_PRINTF_H
-
-#include <stdio.h>
-
-#define printf(format, ...) \
-    (printf(format, ##__VA_ARGS__), fflush(stdout))
-
-#endif
--- pax-tools/readme.md
+++
@@ -1,3 +0,0 @@
-#Pax-Tools
-
-Nástroje a iné programy na pomoc robenia s Paxom 
