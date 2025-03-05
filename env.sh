bc44793dabb0c0bf6bd43719f29c40f47e9cdf7c
Richard Gráčik - Morc <r.gracik@gmail.com>
2025-03-05 12:48:18 +0100

    env.sh: PREFIX fix

--- env.sh
+++ env.sh
@@ -3,6 +3,6 @@
 export LD_LIBRARY_PATH=$PWD/toolchain/lib
 export QEMU_LD_PREFIX=$PWD/toolchain/arm-none-linux-gnueabi/libc/
 export PATH=$PWD/toolchain/bin:$PATH
-export PREFIX=toolchain/$PWD
+export PREFIX=$PWD/toolchain
 export BUILD=x86_64-linux-gnu
 export HOST=arm-none-linux-gnueabi
