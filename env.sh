9da88f1bce12653111c5e049d1c8edab21105f05
Richard Gracik <r.gracik@gmail.com>
2025-03-07 17:42:37 +0100

    env.sh: usage msg for paxpush&paxpull + directory support for paxpush

--- env.sh
+++ env.sh
@@ -10,10 +10,47 @@ export BUILD=x86_64-linux-gnu
 export HOST=arm-none-linux-gnueabi
 export PAXPATH=$PWD
 
+function retry_cmd() {
+	local attempt=0
+
+	while [ $attempt -le 10 ]; do
+		eval "$1" && return 0
+		exit_code=$?
+
+		if [ $exit_code -eq $2 ]; then
+			echo "attempt $attempt failed, retrying in 3 seconds..."
+			sleep 3
+			((attempt++))
+		fi
+	done
+
+	echo "failed after 10 attempts..."
+	return
+}
+
 function paxpush() {
- 	 python3 $PAXPATH/xcb/client.py h push $1 $2
+	if [ "$#" -lt 2 ]; then
+		echo "usage: paxpush <source file> <pax destination>"
+		return
+	fi
+
+	if [ -d "$1" ]; then
+		find "$1" -type f -print0 | while IFS= read -r -d '' file; do
+			relative="${file#$1/}"
+			printf "pushing $file to $2/$relative "
+			retry_cmd "python3 $PAXPATH/xcb/client.py h push '$file' '$2/$relative'" 1
+			sleep 2
+		done
+	else
+		retry_cmd "python3 $PAXPATH/xcb/client.py h push '$1' '$2'" 1
+	fi
 }
 
 function paxpull() {
- 	 python3 $PAXPATH/xcb/client.py h pull $1 $2
+	if [ "$#" -lt 2 ]; then
+		echo "usage: paxpull <pax source> <local destination>"
+		return
+	fi
+
+	retry_cmd "python3 $PAXPATH/xcb/client.py h pull '$1' '$2'" 1
 }
