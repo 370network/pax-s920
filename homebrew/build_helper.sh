e06a97ebdb3aea8a1345e925ba8b5f212e93a0ac
Richard Gracik <r.gracik@gmail.com>
2025-03-07 17:06:42 +0100

    homebrew/build_helper.sh: check&copy dir, patch command

--- homebrew/build_helper.sh
+++ homebrew/build_helper.sh
@@ -8,3 +8,29 @@ build_check () {
                 echo "file: $2.so"
         fi
 }
+
+build_dir_check () {
+        if [ -d $1 ]; then
+                echo "$2 built successfully!"
+                mv $1 $2
+                echo "directory: $2"
+        fi
+}
+
+build_patch () {
+	echo "applying patch $1..."
+
+	if [ -f "$1.patched" ]; then
+		echo "patch $1 already applied!"
+		return
+	fi
+
+	patch -d $2 -p1 < $1
+
+	if [ $? -eq 0 ]; then
+		echo "successfully applied patch $1"
+		touch "$1.patched"
+	else
+		echo "failed to apply patch $1"
+	fi
+}
