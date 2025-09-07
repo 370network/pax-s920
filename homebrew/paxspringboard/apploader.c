ee94d4068838c232b9a76f6c68ec40d41b63d1c7
Ion Agorria <ion@agorria.com>
2025-09-07 15:52:58 +0200

    homebrew/launcher: Create a forked process for dlopen This fixed reopening same app not doing anything issue and also can prevent .so bugs killing the main process

--- homebrew/paxspringboard/apploader.c
+++ homebrew/paxspringboard/apploader.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h> 
 
 int LoadApp(const char *appName) {
-    printf("Loading app: %s\n", appName);
     void *handle;
-    char *error;
-
+    char app_path[256];
+    char so_path[256];
+    pid_t forked_pid;
+    int status = 0;
+    printf("Loading app: %s\n", appName);
     
-    printf("stage 1...\n");
-    char path[256];
-    snprintf(path, sizeof(path), "/data/app/MAINAPP/apps/%s/%s.so", appName, appName);
-    handle = dlopen(path, RTLD_LAZY);
-    if (!handle) {
-        printf("dlopen failed: %s\n", dlerror());
-        return 1;
-    }
+    //We need to fork the process, otherwise doing dlopen the second time will not reload the lib as is already resident
+    forked_pid = fork();
+    if (forked_pid == 0) {
+        //Forked process, launch lib
 
-    dlclose(handle);
+        //Set current dir
+        snprintf(app_path, sizeof(app_path), "/data/app/MAINAPP/apps/%s", appName);
+        chdir(app_path);
+
+        //dlopen the so
+        snprintf(so_path, sizeof(so_path), "%s/%s.so", app_path, appName);
+        handle = dlopen(so_path, RTLD_LAZY | RTLD_LOCAL);
+        if (!handle) {
+            printf("dlopen failed: %s\n", dlerror());
+            return 1;
+        }
+        dlclose(handle);
+        
+        //Terminate the forked process
+        exit(0);
+    } else if (forked_pid > 0) {
+        waitpid(forked_pid, &status, 0);
+        printf("App PID %d status %d\n", forked_pid, status);
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);        
+            if (exit_status != 0) {
+                printf("App exited with %d code\n", exit_status);
+            }
+        } else {
+            printf("App terminated unexpectly\n");
+        }
+    } else {
+        printf("Error: fork failed %d\n", forked_pid);
+    }
+    
     return 0;
 }
