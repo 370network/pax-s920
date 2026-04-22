0a147b26afaab84e23a509029a7d5b4f8e4ead44
TheMorc <r.gracik@gmail.com>
2026-04-23 00:02:14 +0200

    env.sh: additional preparations for the new toolchain

--- env.sh
+++ env.sh
@@ -2,8 +2,6 @@
 # pax-build envset
 # 2nd revision | 2026
 
-source xcb/bin/activate
-
 export env_platform=$(uname)
 if [ "$env_platform" = "Linux" ]; then
 	env_platform="linux-gnu"
@@ -16,13 +14,41 @@ if [ "$env_arch" = "arm64" ]; then
 	env_arch="aarch64"
 fi
 
-export LD_LIBRARY_PATH=$PWD/toolchain/lib
-export QEMU_LD_PREFIX=$PWD/toolchain/arm-unknown-linux-gnueabi2.13/lib/
-export PATH=$PWD/toolchain/bin:$PATH
-export PREFIX=$PWD/toolchain
+#script path
+if [ "$env_platform" = "linux-gnu" ]; then
+	export PAXPATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+elif [ "$env_platform" = "apple-darwin" ]; then
+	export PAXPATH=${0:A:h}
+else
+	export PAXPATH=$PWD
+fi
+
+source $PAXPATH/xcb/bin/activate
+
+glibc_string=2.13
+CC=$HOST$glibc_string-gcc
+CXX=$HOST$glibc_string-g++
+export CC=$CC
+export CXX=$CXX
+export AS=$HOST-as
+export AR=$HOST-ar
+export LD=$HOST-ld
+export NM=$HOST-nm
 export BUILD=$env_arch-$env_platform
-export HOST=arm-unknown-linux-gnueabi2.13
-export PAXPATH=$PWD
+export HOST=arm-unknown-linux-gnueabi
+export PATH=$PAXPATH/toolchain/bin:$PATH
+export PREFIX=$PAXPATH/toolchain
+export LD_LIBRARY_PATH=$PAXPATH/toolchain/lib
+export QEMU_LD_PREFIX=$PAXPATH/toolchain/arm-unknown-linux-gnueabi2.13/lib/
+export CPPFLAGS="--sysroot=$PAXPATH/toolchain/arm-unknown-linux-gnueabi -I$PAXPATH/toolchain/include"
+export CXXFLAGS=$CPPFLAGS
+export CFLAGS=$CPPFLAGS
+export CROSS_COMPILE_TRIPLET='arm-unknown-linux-gnueabi'
+export CROSS_COMPILE_SYSTEM='linux'
+export CROSS_COMPILE_ARCHITECTURE='arm'
+export CROSS_COMPILE_GLIBCVER='2.13'
+export CROSS_COMPILE_SYSROOT=$PAXPATH/toolchain/arm-unknown-linux-gnueabi2.13
+export OBGGCC_HOME=$PAXPATH/toolchain/
 
 function paxreconfigure() {
 	printf "[*] Enter device address ({IP|hostname}+port or the whole /dev/ path): "
@@ -77,8 +103,8 @@ function paxdump() {
 	python3 $PAXPATH/xcb/src/main.py dump $1 $2
 }
 
-if [ ! -f .xcb_config ]; then
+if [ ! -f $PAXPATH/.xcb_config ]; then
 	paxreconfigure
 else
-	source .xcb_config
+	source $PAXPATH/.xcb_config
 fi
