f0808b1dea6bf0b2f5a5fe8de6bd04db22e5bc25
TheMorc <r.gracik@gmail.com>
2026-04-23 19:24:23 +0200

    homebrew: build_helper.cmake MilkyTrackerPAX errata + slight readjustments

--- homebrew/build_helper.cmake
+++ homebrew/build_helper.cmake
@@ -1,13 +1,15 @@
 set(PREFIX $ENV{PREFIX})
 set(HOST $ENV{HOST})
 
+set(TARGET_GLIBC "2.13")
+set(TOOLCHAIN_PREFIX "$arm-unknown-linux-gnueabi")
+set(TOOLCHAIN_PATH "$ENV{PREFIX}")
 
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_C_COMPILER   ${HOST}-gcc)
-set(CMAKE_CXX_COMPILER ${HOST}-g++)
+set(CMAKE_C_COMPILER   ${CC})
+set(CMAKE_CXX_COMPILER ${CXX})
 
-set(CMAKE_FIND_ROOT_PATH  ${PREFIX} ${PREFIX}/arm-none-linux-gnueabi)
-
+set(CMAKE_FIND_ROOT_PATH  ${PREFIX} ${PREFIX}/arm-unknown-linux-gnueabi)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
@@ -22,6 +24,11 @@ if(ERRATA_2)
 	#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0")
 	#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden")
 	#set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++ -static-libgcc")
+endif()
+
+if(ERRATA_3)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,relro")
 endif()
 
 add_definitions(-include ${PREFIX}/include/prolin_printf.h)
