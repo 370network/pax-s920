d26e064de759368433fa94869ba28efa7205af8b
TheMorc <r.gracik@gmail.com>
2026-04-23 13:15:36 +0200

    homebrew: examples: osal_test add gradient

--- homebrew/examples/osal_test.c
+++ homebrew/examples/osal_test.c
@@ -1,5 +1,5 @@
 #include <dlfcn.h>
-#include <stdlib.h>
+#include <fcntl.h>
 
 // lib symbol loader
 typedef void (*generic_func)();
@@ -8,21 +8,39 @@ generic_func load_symbol(void *lib_handle, const char *symbol_name) {
     return symbol;
 }
 
+static unsigned short *lcd_fb;
+
 int _init()
 {
-        void *libosal = dlopen("/usr/lib/libosal.so", RTLD_LAZY);
-
-        void (*OsScrBrightness)() = load_symbol(libosal, "OsScrBrightness");
-
-        int i;
-        for(i = 0; i < 10; i++)
-        {
-                printf("Setting screen brightness to: %d\n", i);
-                OsScrBrightness(i);
-                sleep(1);
-        }
-
-        return(0);
+		printf("OSAL Test!\n");
+		
+		int fb_fd = open ("/dev/fb", O_RDWR);
+		lcd_fb = mmap (0, 320 * 240 * 2, 3, 1, fb_fd, 0);
+		
+		int i, j;
+		for (i = 0; i < 240; i++)
+			for (j = 0; j < 320; j++)
+			{
+				int r = (j * 31) / 319;
+				int g = (i * 63) / 239;
+				int b = 15;
+				
+				lcd_fb[j + i * 320] = (r << 11) | (g << 5) | (b);
+			}
+		
+		void *libosal = dlopen("/usr/lib/libosal.so", RTLD_LAZY);
+		
+		void (*OsScrBrightness)() = load_symbol(libosal, "OsScrBrightness");
+		
+		i = 0;
+		for(i = 0; i <= 10; i++)
+		{
+			printf("Setting screen brightness to: %d\n", i);
+			OsScrBrightness(i);
+			sleep(1);
+		}
+		
+		return(0);
 }
 
 
