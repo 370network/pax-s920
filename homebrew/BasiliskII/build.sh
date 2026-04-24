15836d07574c5005c21354cafcd99eac037b1734
TheMorc <r.gracik@gmail.com>
2026-04-24 22:47:07 +0200

    homebrew: BasiliskII: linux compatible build.sh

--- homebrew/BasiliskII/build.sh
+++ homebrew/BasiliskII/build.sh
@@ -7,13 +7,20 @@ echo "370network paxdevs"
 echo "=build BasiliskII="
 echo "=================="
 
-git clone https://github.com/kanjitalk755/macemu
-
-if [ ! -d repo ]; then
-	cp -r macemu/BasiliskII repo
+if [ $env_platform != "linux*" ]; then
+	echo "You are currently building on $env_platform, this environment is UNSUPPORTED!"
+	echo "BasiliskII compilation process currently depends on a Linux environment with qemu-binfmt!"
+	echo "If you want to compile BasiliskII, you have to do so elsewhere. Sorry :("
+	exit
 fi
 
-if [ ! -f repo/build/src/tracker/libmilkytracker.so ]; then
+git clone https://github.com/kanjitalk755/macemu repo-macemu
+
+if [ ! -d repo ]; then
+	cp -r repo-macemu/BasiliskII repo
+fi
+
+if [ ! -f repo/src/Unix/BasiliskII ]; then
 	echo "BasiliskII build process!"
 
 	cd repo/src/Unix
@@ -24,7 +31,10 @@ if [ ! -f repo/build/src/tracker/libmilkytracker.so ]; then
 	unset AS
 	unset STRIP
 	unset LD_LIBRARY_PATH
+	unset CPPFLAGS
+	unset LDFLAGS
+	unset LIBS
 	NO_CONFIGURE=1 ./autogen.sh
-	./configure --host=$HOST_GLIBC --prefix=$PREFIX --with-sdl1 CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/SDL" LDFLAGS="-L$PREFIX/lib" LIBS="-lSDL"
+	./configure --host=$HOST_GLIBC --prefix=$PREFIX --with-sdl1 BII_CROSS_HAVE_EXTENDED_SIGNALS=yes CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/SDL" LDFLAGS="-L$PREFIX/lib" LIBS="-lSDL"
 	make -j$(nproc)
 fi
